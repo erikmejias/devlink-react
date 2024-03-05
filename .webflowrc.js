775ac447ff0f89f8db0d5fb859797555516e5cc8
Erik Mejia <erik@erikmejia.com>
2024-03-05 11:54:27 -0400

    feat: add signals dependency

--- .webflowrc.js
+++ .webflowrc.js
@@ -2,9 +2,9 @@ export default {
   host: "https://api.webflow.com",
   rootDir: "./devlink",
   siteId: process.env.WEBFLOW_SITE_ID,
-  authToken: process.env.WEBFLOW_AUTH_TOKEN, // An environment variable is recommended for this field.
+  authToken: process.env.WEBFLOW_AUTH_TOKEN,
   cssModules: true,
   fileExtensions: {
-    js: "jsx",
+    js: "jsx", // Prefer to use JSX instead of JS
   },
 };
